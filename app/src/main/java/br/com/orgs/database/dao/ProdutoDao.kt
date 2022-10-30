9811d0cee149a0b740a346ed81a50782c9d758c5
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-10-30 19:51:59 -0300

    Adicionando a classe Converter

---
+++ app/src/main/java/br/com/orgs/database/dao/ProdutoDao.kt
@@ -0,0 +1,16 @@
+package br.com.orgs.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import br.com.orgs.model.Produto
+
+@Dao
+interface ProdutoDao {
+
+    @Query("SELECT *FROM Produto")
+    fun buscaTodos(): List<Produto>
+
+    @Insert
+    fun salva(produto: Produto)
+}
